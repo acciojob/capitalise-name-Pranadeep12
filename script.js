9cc31ef6dad74a6b2843befba9fdf4a7691a6ce7
Pranadeep12 <129034610+Pranadeep12@users.noreply.github.com>
2025-08-31 18:38:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+// Get the input field
+const inputField = document.getElementById("fname");
+
+// Add blur event (when user leaves the input field)
+inputField.addEventListener("blur", function() {
+  inputField.value = inputField.value.toUpperCase();
+});
